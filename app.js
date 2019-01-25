609ad6aef453ca77d8877a26b57d7ce8a783691e
Acydh <spadavecchia.andrea@spes.uniud.it>
2019-01-25 12:35:55 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,71 @@
+// jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  var firstName = req.body.firstName;
+  var lastName = req.body.lastName;
+  var email = req.body.email;
+
+  var data = JSON.stringify({
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+      }
+    }]
+  });
+
+  app.post("/failure", function(req, res){
+
+  });
+
+  var options = {
+    url: "https://us17.api.mailchimp.com/3.0/lists/ba9ed36ab7",
+    method: "POST",
+    headers: {
+      authorization: "acydh 322e537019542317ad8ef5d4f227b671-us17"
+    },
+    //body: data
+  };
+
+  request(options, function(error, response, body) {
+    if (error) {
+      res.sendFile(__dirname + "/failure.html");
+    } else {
+      if (response.statusCode === 200){
+        res.sendFile(__dirname + "/success.html");
+      } else {
+        res.sendFile(__dirname + "/failure.html");
+      }
+    }
+    console.log(response.statusCode);
+  });
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+
+
+
+app.listen(3000, function() {
+  console.log("server is running on port 3000");
+});
